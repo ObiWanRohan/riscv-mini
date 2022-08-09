d206f4893ae3cd949e5a435ff427857c2d670cc5
ang_pop_os <aakashnareshgupta@gmail.com>
2022-08-09 23:30:33 +0530

    added RV constants and decode forward logic

--- src/main/scala/mini/ForwardingUnit.scala
+++ src/main/scala/mini/ForwardingUnit.scala
@@ -4,37 +4,32 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental.ChiselEnum
 
+import RISCVConstants._
+
 // Forwarding options for Decode stage
 object ForwardDecOperand extends ChiselEnum {
   // Don't forward anything
   val FWD_NONE = Value(0.U)
-  // Forward value from EW pipeline register
+  // Forward value from EM pipeline register
   val FWD_EM = Value(2.U)
+  // Forward value from MW pipeline register
+  val FWD_MW = Value(3.U)
 }
 
 // Forwarding options for Execute stage
 object ForwardExeOperand extends ChiselEnum {
   // Don't forward anything
   val FWD_NONE = Value(0.U)
-  // Forward value from EW pipeline register
+  // Forward value from EM pipeline register
   val FWD_EM = Value(2.U)
-}
-
-object ForwardMemLoad extends ChiselEnum {
-  // Don't forward anyting
-  val FWD_NONE = Value(0.U)
-  // forward the load data from mem back to Execute stage Input
-  val FWD_LD = Value(2.U)
+  // Forward value from MW pipeline register
+  val FWD_MW = Value(3.U)
 }
 
 class ForwardingUnitIO(width: Int) extends Bundle {
 
   import mini.{DecodeExecutePipelineRegister, ExecuteMemoryPipelineRegister, MemoryWritebackPipelineRegister, FetchDecodePipelineRegister}
   import CPUControlSignalTypes._
-
-  // Can add a global constant for the register address width
-
-  val REG_ADDR_WIDTH = 5.W
 
   /* Inputs from pipeline registers
    */
@@ -76,8 +71,8 @@ class ForwardingUnit(width: Int) extends Module {
   // val rs1hazard = wb_en && rs1_addr.orR && (rs1_addr === wb_rd_addr)
   // val rs2hazard = wb_en && rs2_addr.orR && (rs2_addr === wb_rd_addr)
 
-  val dec_rs1_addr = io.fd_reg.inst(19, 15)
-  val dec_rs2_addr = io.fd_reg.inst(24, 20)
+  val dec_rs1_addr = io.fd_reg.inst(RS1_MSB, RS1_LSB)
+  val dec_rs2_addr = io.fd_reg.inst(RS2_MSB, RS2_LSB)
 
   // Condition for forwarding for DE pipeline reg
   // This bypasses the hazard between the Decode and Writeback stage
@@ -86,10 +81,15 @@ class ForwardingUnit(width: Int) extends Module {
   io.forward_dec_opA := MuxCase(
     ForwardDecOperand.FWD_NONE,
     IndexedSeq(
-      (
+      ( // For bypass from WB stage to DEC. This happens regardless of type of writeback
         io.wb_rd === dec_rs1_addr    // The destination register is the same that is being read
         && dec_rs1_addr.orR         // The destination register is not register x0
         && io.wb_en                   // Writeback is enabled
+      ) -> ForwardDecOperand.FWD_MW,
+      ( // For bypass from MEM stage to DEC (i.e forwarding the value received from EX stage).
+        io.em_reg.inst(RD_MSB, RD_LSB) === dec_rs1_addr    // The destination register is the same that is being read
+        && dec_rs1_addr.orR         // The destination register is not register x0
+        && io.em_reg.wb_en                   // Writeback is enabled
         && io.wb_sel === WbSel.WB_ALU       // We are writing the ALU value to the register
       ) -> ForwardDecOperand.FWD_EM
     )
@@ -137,6 +137,7 @@ class ForwardingUnit(width: Int) extends Module {
       ) -> ForwardExeOperand.FWD_EM,
       (
         // CONTROL LOGIC FOR LOAD BYPASS
+
       )
     )
   )
