38c88b37ab2563bd3503f26cc1a3017825ff6f10
ang_pop_os <aakashnareshgupta@gmail.com>
2022-11-08 14:57:23 +0530

    initial 2 stage alu implimention

--- src/main/scala/mini/Alu.scala
+++ src/main/scala/mini/Alu.scala
@@ -22,20 +22,21 @@ object AluSel extends ChiselEnum {
   val ALU_XXX = Value(15.U)
 }
 
-class AluIO(width: Int) extends Bundle {
-  val A = Input(UInt(width.W))
-  val B = Input(UInt(width.W))
-  val alu_op = Input(AluSel())
-  val out = Output(UInt(width.W))
-  val sum = Output(UInt(width.W))
-}
+// class AluIO(width: Int) extends Bundle {
+//   val A = Input(UInt(width.W))
+//   val B = Input(UInt(width.W))
+//   val alu_op = Input(AluSel())
+//   val out = Output(UInt(width.W))
+//   val sum = Output(UInt(width.W))
+// }
 class AluStageIO(width: Int) extends Bundle {
   val A = Input(UInt(width.W))
   val B = Input(UInt(width.W))
   val Cin = Input(UInt(1.W))
   val alu_op = Input(AluSel())
-  val Pout = Output(UInt(width.W))
+  val Out = Output(UInt(width.W))
   val Cout = Output(UInt(1.W))
+  val sum = Output(UInt(width.W))
 }
 
 import mini.AluSel._
@@ -45,100 +46,148 @@ trait Alu extends Module {
   val io: AluStageIO
 }
 
-class AluSimple(val width: Int) extends Alu {
-  val io = IO(new AluIO(width))
+// class AluSimple(val width: Int) extends Alu {
+//   val io = IO(new AluIO(width))
 
-  val shamt = io.B(4, 0).asUInt
+//   val shamt = io.B(4, 0).asUInt
 
-  io.out := MuxLookup(
-    io.alu_op.asUInt,
-    io.B,
-    Seq(
-      ALU_ADD.asUInt -> (io.A + io.B),
-      ALU_SUB.asUInt -> (io.A - io.B),
-      ALU_SRA.asUInt -> (io.A.asSInt >> shamt).asUInt,
-      ALU_SRL.asUInt -> (io.A >> shamt),
-      ALU_SLL.asUInt -> (io.A << shamt),
-      ALU_SLT.asUInt -> (io.A.asSInt < io.B.asSInt),
-      ALU_SLTU.asUInt -> (io.A < io.B),
-      ALU_AND.asUInt -> (io.A & io.B),
-      ALU_OR.asUInt -> (io.A | io.B),
-      ALU_XOR.asUInt -> (io.A ^ io.B),
-      ALU_COPY_A.asUInt -> io.A
-    )
-  )
+//   io.out := MuxLookup(
+//     io.alu_op.asUInt,
+//     io.B,
+//     Seq(
+// ALU_ADD.asUInt -> (io.A + io.B),
+// ALU_SUB.asUInt -> (io.A - io.B),
+// ALU_SRA.asUInt -> (io.A.asSInt >> shamt).asUInt,
+// ALU_SRL.asUInt -> (io.A >> shamt),
+// ALU_SLL.asUInt -> (io.A << shamt),
+// ALU_SLT.asUInt -> (io.A.asSInt < io.B.asSInt),
+// ALU_SLTU.asUInt -> (io.A < io.B),
+// ALU_AND.asUInt -> (io.A & io.B),
+// ALU_OR.asUInt -> (io.A | io.B),
+// ALU_XOR.asUInt -> (io.A ^ io.B),
+// ALU_COPY_A.asUInt -> io.A
+//     )
+//   )
 
-  io.sum := io.A + Mux(io.alu_op.asUInt(0), -io.B, io.B)
-}
+//   io.sum := io.A + Mux(io.alu_op.asUInt(0), -io.B, io.B)
+// }
 
-class AluArea(val width: Int) extends Alu {
-  val io = IO(new AluIO(width))
-  val sum = io.A + Mux(io.alu_op.asUInt(0), -io.B, io.B)
-  val cmp =
-    Mux(io.A(width - 1) === io.B(width - 1), sum(width - 1), Mux(io.alu_op.asUInt(1), io.B(width - 1), io.A(width - 1)))
-  val shamt = io.B(4, 0).asUInt
-  val shin = Mux(io.alu_op.asUInt(3), io.A, Reverse(io.A))
-  val shiftr = (Cat(io.alu_op.asUInt(0) && shin(width - 1), shin).asSInt >> shamt)(width - 1, 0)
-  val shiftl = Reverse(shiftr)
+// class AluArea(val width: Int) extends Alu {
+//   val io = IO(new AluIO(width))
+//   val sum = io.A + Mux(io.alu_op.asUInt(0), -io.B, io.B)
+//   val cmp =
+//     Mux(io.A(width - 1) === io.B(width - 1), sum(width - 1), Mux(io.alu_op.asUInt(1), io.B(width - 1), io.A(width - 1)))
+//   val shamt = io.B(4, 0).asUInt
+//   val shin = Mux(io.alu_op.asUInt(3), io.A, Reverse(io.A))
+//   val shiftr = (Cat(io.alu_op.asUInt(0) && shin(width - 1), shin).asSInt >> shamt)(width - 1, 0)
+//   val shiftl = Reverse(shiftr)
 
-  val out =
-    Mux(
-      io.alu_op === ALU_ADD || io.alu_op === ALU_SUB,
-      sum,
-      Mux(
-        io.alu_op === ALU_SLT || io.alu_op === ALU_SLTU,
-        cmp,
-        Mux(
-          io.alu_op === ALU_SRA || io.alu_op === ALU_SRL,
-          shiftr,
-          Mux(
-            io.alu_op === ALU_SLL,
-            shiftl,
-            Mux(
-              io.alu_op === ALU_AND,
-              io.A & io.B,
-              Mux(
-                io.alu_op === ALU_OR,
-                io.A | io.B,
-                Mux(io.alu_op === ALU_XOR, io.A ^ io.B, Mux(io.alu_op === ALU_COPY_A, io.A, io.B))
-              )
-            )
-          )
-        )
-      )
-    )
+//   val out =
+//     Mux(
+//       io.alu_op === ALU_ADD || io.alu_op === ALU_SUB,
+//       sum,
+//       Mux(
+//         io.alu_op === ALU_SLT || io.alu_op === ALU_SLTU,
+//         cmp,
+//         Mux(
+//           io.alu_op === ALU_SRA || io.alu_op === ALU_SRL,
+//           shiftr,
+//           Mux(
+//             io.alu_op === ALU_SLL,
+//             shiftl,
+//             Mux(
+//               io.alu_op === ALU_AND,
+//               io.A & io.B,
+//               Mux(
+//                 io.alu_op === ALU_OR,
+//                 io.A | io.B,
+//                 Mux(io.alu_op === ALU_XOR, io.A ^ io.B, Mux(io.alu_op === ALU_COPY_A, io.A, io.B))
+//               )
+//             )
+//           )
+//         )
+//       )
+//     )
 
-  io.out := out
-  io.sum := sum
-}
+//   io.out := out
+//   io.sum := sum
+// }
 
 class AluStage(val width: Int) extends Alu {
-  val io = IO(new AluStageIO(width / 2))
+  val io = IO(new AluStageIO(width))
 
   // shift-amount
   val shamt = io.B(4, 0).asUInt
-  // partial alu result
-  val presult = Wire(Int(width / 2 + 1))
-  val carry = Mux(io.alu_op === ALU_SUB, 1.U, io.Cin)
+  val presult = Wire(UInt(width.W))
+  val stages = List(2, 5)
+
+  // ADD
+
+  val adder = Module(new FastAdderPipelined(width, stages))
+  // val sum = Wire(UInt((width).W))
+  val cin_sum = Wire(UInt(1.W))
+  cin_sum := io.Cin
+  val cout_sum = Wire(UInt(1.W))
+  adder.io.a := io.A
+  adder.io.b := io.B
+  adder.io.cin := cin_sum
+  io.sum := adder.io.Sum
+  cout_sum := adder.io.Cout
+
+  // SUB
+  val subtractor = Module(new FastSubtractorPipelined(width, stages))
+  val cin_sub = Wire(UInt(1.W))
+  cin_sub := io.Cin
+  val diff = Wire(UInt((width).W))
+  val cout_sub = Wire(UInt(1.W))
+  subtractor.io.a := io.A
+  subtractor.io.b := io.B
+  subtractor.io.cin := cin_sub
+  diff := subtractor.io.Diff
+  cout_sub := subtractor.io.Cout
+
+  // SRA
+  // val shifterRA = new BarrelShifterRightArithmetic(width)
+  val shiftRA = Wire(UInt((width).W))
+  shiftRA := RegNext((io.A.asSInt >> shamt).asUInt)
+  // shifterRA.io.In := io.A
+  // shifterRA.io.Shamt := shamt
+  // shiftRA := shifterRA.io.Out
+
+  // // SRL
+  // val ShifterRL = new BarrelShifterRightLogical(width)
+  // val shiftRL = Wire(UInt((width).W))
+  // shifterRL.io.In := io.a
+  // shifterRL.io.Shamt := shamt
+  // shiftRL := shifterRL.io.Out
+
+  // val ShifterLL = new BarrelShifterLeftLogical(width)
+  // val shiftLL = Wire(UInt((width).W))
+  // shifterLL.io.In := io.a
+  // shifterLL.io.Shamt := shamt
+  // shiftLL := shifterLL.io.Out
+
+  // val lessThan impliment using subtractor?
 
   presult := MuxLookup(
     io.alu_op.asUInt,
     io.B,
     Seq(
       // use alu_utils for custom HW implimentaitons -- barrel shifter, parallel-prefix add/sub
-      ALU_ADD.asUInt -> (io.A + io.B + io.Cin),
-      ALU_SUB.asUInt -> (io.A + ~io.B + carry),
-      ALU_SRA.asUInt -> (io.A.asSInt >> shamt).asUInt,
-      ALU_SRL.asUInt -> (io.A >> shamt),
-      ALU_SLL.asUInt -> (io.A << shamt),
-      ALU_SLT.asUInt -> (io.A.asSInt < io.B.asSInt),
-      ALU_SLTU.asUInt -> (io.A < io.B),
-      ALU_AND.asUInt -> (io.A & io.B),
-      ALU_OR.asUInt -> (io.A | io.B),
-      ALU_XOR.asUInt -> (io.A ^ io.B),
-      ALU_COPY_A.asUInt -> io.A
+      ALU_ADD.asUInt -> (io.sum),
+      ALU_SUB.asUInt -> (diff),
+      ALU_SRA.asUInt -> (shiftRA),
+      ALU_SRL.asUInt -> RegNext((io.A >> shamt)),
+      ALU_SLL.asUInt -> RegNext((io.A << shamt)),
+      ALU_SLT.asUInt -> RegNext((io.A.asSInt < io.B.asSInt)),
+      ALU_SLTU.asUInt -> RegNext((io.A < io.B)),
+      ALU_AND.asUInt -> RegNext((io.A & io.B)),
+      ALU_OR.asUInt -> RegNext((io.A | io.B)),
+      ALU_XOR.asUInt -> RegNext((io.A ^ io.B)),
+      ALU_COPY_A.asUInt -> RegNext(io.A)
     )
   )
-  io.Pout := presult(width / 2 - 1, 0)
-  io.Cout := presult(width / 2)
+  io.Out := presult
+  io.Cout := presult(width - 1)
+  // io.sum := UInt(0.W)
 }
--- src/main/scala/mini/AluUtils.scala
+++ src/main/scala/mini/AluUtils.scala
@@ -1,80 +1,6 @@
 package mini
-
 import chisel3._
 import chisel3.util._
-import chisel3.experimental.ChiselEnum
-
-class AluUtils(width: Int) extends Module {
-  // GPT, Reduce
-  // Parallel Prefix Adder/Subtractor
-  // Barrel Shifter - import verilog blackbox
-}
-
-class FastAddSubPipelined(val width: Int) extends Module {
-  val io = IO(new Bundle {
-    val a = Input(UInt(width.W))
-    val b = Input(UInt(width.W))
-    val cin = Input(UInt(1.W))
-    val Sub = Input(Bool())
-
-    val Result = Output(UInt(width.W))
-    val Cout = Output(UInt(1.W))
-  })
-
-  val gpts_0 = GPTGen(width)(io.a, io.b)
-
-  val groups = VecInit(gpts_0.grouped(4).toVector.map {
-    case group =>
-      ReduceNibble(VecInit(group))
-  })
-
-  def reduce(gpts: Vec[Vec[GPT]], cin: Bool): (Vec[Vec[GPT]], Bool) = {
-    def nextLayer(
-      prev:   Vec[Vec[GPT]],
-      cin:    Bool,
-      offset: Int
-    ): (Vec[Vec[GPT]], Bool) = {
-      if (offset > prev.size) {
-        (RegNext(prev), RegNext(cin))
-      } else {
-        val layer = VecInit(Vector.tabulate(prev.size) { i =>
-          if (i < offset) {
-            prev(i)
-          } else {
-            val (grpi, grpj) = (prev(i), prev(i - offset))
-            val gptj = grpj.last
-            VecInit(grpi.map {
-              case gpti =>
-                GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t)
-            })
-          }
-        })
-
-        val reg = RegNext(layer)
-        val cinreg = RegNext(cin)
-        nextLayer(reg, cinreg, offset << 1)
-      }
-    }
-
-    nextLayer(gpts, cin, 1)
-  }
-
-  val (grps_last, cin) = reduce(groups, io.cin.asBool)
-  val gpts_last = grps_last.flatten
-
-  val gs = gpts_last.map(_.g)
-  val ps = gpts_last.map(_.p)
-  val ts = gpts_last.map(_.t)
-
-  val cs = cin +: gs.zip(ps).map { case (gi, pi) => gi | (pi & cin) }
-
-  val ss = for (i <- 0 until width) yield {
-    cs(i) ^ ts(i)
-  }
-
-  io.Result := RegNext(Mux((Sub, ~VecInit(ss).asUInt), VecInit(ss).asUInt))
-  io.Cout := RegNext(cs.last)
-}
 
 class GPT extends Bundle {
   val g = UInt(1.W)
@@ -117,40 +43,228 @@ object GPTGen {
     mod.io.gpts
   }
 }
+object ParallelPrefixTree {
+  def apply[T](summands: Seq[T])(associativeOp: (T, T) => T): Vector[T] = {
+    def helper(offset: Int, x: Vector[T]): Vector[T] = {
+      if (offset > x.size) {
+        x
+      } else {
+        val layer = Vector.tabulate(x.size) { i =>
+          if (i < offset) {
+            x(i)
+          } else {
+            associativeOp(x(i), x(i - offset))
+          }
+        }
 
-class ReduceNibble extends Module {
-  val io = IO(new Bundle {
-    val gptin = Input(Vec(4, new GPT))
-    val gptout = Output(Vec(4, new GPT))
-  })
+        helper(offset << 1, layer)
+      }
+    }
 
-  val gptin0 = io.gptin(0)
-  val gptout0 = gptin0
-
-  val gptin1 = io.gptin(1)
-  val gptout1 = GPTInit(gptin1.g | (gptin0.g & gptin1.p), gptin1.p & gptin0.p, gptin1.t)
-
-  val gptin2 = io.gptin(2)
-  val gptout2 = GPTInit(
-    gptin2.g | (gptin2.p & gptin1.g) | (gptin0.g & gptin1.p & gptin2.p),
-    gptin2.p & gptin1.p & gptin0.p,
-    gptin2.t
-  )
-
-  val gptin3 = io.gptin(3)
-  val gptout3 = GPTInit(
-    gptin3.g | (gptin3.p & gptin2.g) | (gptin3.p & gptin2.p & gptin1.g) | (gptin0.g & gptin1.p & gptin2.p & gptin3.p),
-    gptin3.p & gptin2.p & gptin1.p & gptin0.p,
-    gptin3.t
-  )
-
-  io.gptout := VecInit(gptout0, gptout1, gptout2, gptout3)
+    helper(1, summands.toVector)
+  }
 }
 
-object ReduceNibble {
-  def apply(gptin: Vec[GPT]) = {
-    val mod = Module(new ReduceNibble)
-    mod.io.gptin := gptin
+class Reduce(val width: Int, val valency: Int) extends Module {
+  val io = IO(new Bundle {
+    val gptin = Input(Vec(width, new GPT))
+    val gptout = Output(Vec(width, new GPT))
+  })
+
+  if (valency == 1) {
+    val gpts = ParallelPrefixTree(io.gptin) {
+      case (gpti, gptj) => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t)
+    }
+    io.gptout := VecInit(gpts)
+  } else {
+    val groups = io.gptin.grouped(valency).toVector.map { case group => Reduce(valency, 1)(VecInit(group)) }
+    val gpts = ParallelPrefixTree(groups) {
+      case (grpi, grpj) => {
+        val gptj = grpj.last
+        VecInit(grpi.map { case gpti => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t) })
+      }
+    }.flatten
+    io.gptout := VecInit(gpts)
+  }
+}
+
+object Reduce {
+  def apply(width: Int, valency: Int)(gpts: Vec[GPT]) = {
+    val mod = Module(new Reduce(width, valency))
+    mod.io.gptin := gpts
     mod.io.gptout
   }
 }
+class FastAdderPipelined(val width: Int, val stages: List[Int]) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(width.W))
+    val b = Input(UInt(width.W))
+    val cin = Input(UInt(1.W))
+
+    val Sum = Output(UInt(width.W))
+    val Cout = Output(UInt(1.W))
+  })
+
+  val gpts_0 = GPTGen(width)(io.a, io.b)
+
+  val groups = VecInit(gpts_0.grouped(4).toVector.map {
+    case group =>
+      VecInit(ParallelPrefixTree(group) {
+        case (gpti, gptj) => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t)
+      })
+  })
+
+  def reduce(gpts: Vec[Vec[GPT]], cin: Bool): (Vec[Vec[GPT]], Bool) = {
+    def nextLayer(prev: Vec[Vec[GPT]], cin: Bool, offset: Int, depth: Int): (Vec[Vec[GPT]], Bool) = {
+      if (offset > prev.size) {
+        if (stages.contains(depth))
+          (RegNext(prev), RegNext(cin))
+        else (prev, cin)
+      } else {
+        val layer = VecInit(Vector.tabulate(prev.size) { i =>
+          if (i < offset) {
+            prev(i)
+          } else {
+            val (grpi, grpj) = (prev(i), prev(i - offset))
+            val gptj = grpj.last
+            VecInit(grpi.map { case gpti => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t) })
+          }
+        })
+
+        val reg = if (stages.contains(depth)) RegNext(layer) else layer
+        val cinreg = if (stages.contains(depth)) RegNext(cin) else cin
+        nextLayer(reg, cinreg, offset << 1, depth + 1)
+      }
+    }
+
+    nextLayer(gpts, cin, 1, 1)
+  }
+
+  val (grps_last, cin) = reduce(groups, io.cin.asBool)
+  val gpts_last = grps_last.flatten
+
+  val gs = gpts_last.map(_.g)
+  val ps = gpts_last.map(_.p)
+  val ts = gpts_last.map(_.t)
+
+  val cs = cin +: gs.zip(ps).map { case (gi, pi) => gi | (pi & cin) }
+
+  val ss = for (i <- 0 until width) yield {
+    cs(i) ^ ts(i)
+  }
+
+  io.Sum := VecInit(ss).asUInt
+  io.Cout := cs.last
+}
+
+class FastSubtractorPipelined(val width: Int, val stages: List[Int]) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(width.W))
+    val b = Input(UInt(width.W))
+    val cin = Input(UInt(1.W))
+
+    val Diff = Output(UInt(width.W))
+    val Cout = Output(UInt(1.W))
+  })
+
+  val gpts_0 = GPTGen(width)(~io.a, io.b)
+
+  val groups = VecInit(gpts_0.grouped(4).toVector.map {
+    case group =>
+      VecInit(ParallelPrefixTree(group) {
+        case (gpti, gptj) => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t)
+      })
+  })
+
+  def reduce(gpts: Vec[Vec[GPT]], cin: Bool): (Vec[Vec[GPT]], Bool) = {
+    def nextLayer(prev: Vec[Vec[GPT]], cin: Bool, offset: Int, depth: Int): (Vec[Vec[GPT]], Bool) = {
+      if (offset > prev.size) {
+        if (stages.contains(depth))
+          (RegNext(prev), RegNext(cin))
+        else (prev, cin)
+      } else {
+        val layer = VecInit(Vector.tabulate(prev.size) { i =>
+          if (i < offset) {
+            prev(i)
+          } else {
+            val (grpi, grpj) = (prev(i), prev(i - offset))
+            val gptj = grpj.last
+            VecInit(grpi.map { case gpti => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t) })
+          }
+        })
+
+        val reg = if (stages.contains(depth)) RegNext(layer) else layer
+        val cinreg = if (stages.contains(depth)) RegNext(cin) else cin
+        nextLayer(reg, cinreg, offset << 1, depth + 1)
+      }
+    }
+
+    nextLayer(gpts, cin, 1, 1)
+  }
+
+  val (grps_last, cin) = reduce(groups, io.cin.asBool)
+  val gpts_last = grps_last.flatten
+
+  val gs = gpts_last.map(_.g)
+  val ps = gpts_last.map(_.p)
+  val ts = gpts_last.map(_.t)
+
+  val cs = cin +: gs.zip(ps).map { case (gi, pi) => gi | (pi & cin) }
+
+  val ss = for (i <- 0 until width) yield {
+    cs(i) ^ ts(i)
+  }
+
+  io.Diff := VecInit(ss).asUInt
+  io.Cout := cs.last
+}
+
+class ShiftRight(val width: Int) extends Module {
+  val shiftw = log2Up(width)
+  val io = IO(new Bundle {
+    val a = Input(UInt(width.W))
+    val shamt = Input(UInt(shiftw.W))
+
+    val out = Output(UInt(width.W))
+  })
+
+  def tree(data: UInt, shamt: UInt): UInt = {
+    def nextLayer(prev: UInt, idx: Int): UInt = {
+      if (idx < 0) {
+        prev
+      } else {
+        nextLayer(Mux(shamt(idx), prev >> (1 << idx), prev), idx - 1)
+      }
+    }
+
+    nextLayer(data, shiftw - 1)
+  }
+
+  io.out := tree(io.a, io.shamt)
+}
+
+class ShiftLeft(val width: Int) extends Module {
+  val shiftw = log2Up(width)
+  val io = IO(new Bundle {
+    val a = Input(UInt(width.W))
+    val shamt = Input(UInt(shiftw.W))
+
+    val out = Output(UInt(width.W))
+  })
+
+  def tree(data: UInt, shamt: UInt): UInt = {
+    def nextLayer(prev: UInt, idx: Int): UInt = {
+      if (idx < 0) {
+        prev
+      } else {
+        nextLayer(Mux(shamt(idx), prev << (1 << idx), prev), idx - 1)
+      }
+    }
+
+    nextLayer(data, shiftw - 1)
+  }
+
+  io.out := tree(io.a, io.shamt)
+}
+
+// z-extend for SRA -- shift right arithmetic
